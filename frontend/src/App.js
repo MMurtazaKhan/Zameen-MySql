941990295ce3ea98a3da5dda6c71e00c9aa60e8f
muhammadabdullahimdad <muhammadabdullahimdad@gmail.com>
2022-06-30 00:09:54 +0500

    admin-panel-added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,14 +17,11 @@ import RegisterProperty from "./pages/RegisterProperty/RegisterProperty";
 import EditProfile from "./pages/User/EditProfile/EditProfile";
 import Booking from "./pages/BookingProperty/Booking";
 import Sell from "./pages/Sell/Sell";
-<<<<<<< HEAD
-import propertyDetails from "./pages/Sell/propertyDetails";
-=======
 import ProjectProperty from './pages/BookingProperty/ProjectProperty';
 import ProjectDetail from './pages/BookingProperty/ProjectDetail';
 import sellDetail from './pages/Sell/sellDetail';
+import Dashboard from './pages/Admin/Dashboard/Dashboard';
 
->>>>>>> b49c442982fc9dc3d37545f71bb4cc7a6ec68846
 
 
 
@@ -52,13 +49,10 @@ function App() {
           <Route exact path="/EditProfile" component={EditProfile} />
           <Route exact path="/book-property" component={Booking} />
           <Route exact path="/sell" component={Sell} />
-<<<<<<< HEAD
-          <Route exact path="/property/:id" component={propertyDetails} />
-=======
           <Route exact path="/sellDetail/:id" component={sellDetail} />
           <Route exact path="/projects" component={ProjectProperty} />
           <Route exact path="/projectDetail/:id" component={ProjectDetail} />
->>>>>>> b49c442982fc9dc3d37545f71bb4cc7a6ec68846
+          <Route exact path="/admin/dashboard" component={Dashboard} />
 
           
         </Switch>
---
+++ frontend/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <Sidebar />
+
+      <div className="dashboardContainer"></div>
+    </div>
+  );
+};
+
+export default Dashboard;
