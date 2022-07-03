efa3095291a2f34ffd240e4f64e59c4a44f349e6
afourabdullah <abdullahazees4gmail.com>
2022-07-03 14:06:32 +0500

    blogdetailpage submitted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React
   // ,{ useEffect }
   // , { useEffect, useState }
@@ -7,14 +7,10 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import About from "./pages/AboutUs/About";
 import Blog from './pages/Blog/BlogMain';
-=======
-import React from 'react';
-// ,{ useEffect }
-// , { useEffect, useState }
+import BlogViewDetails from "./pages/Blog/BlogViewDetails";
 
- import "./App.css";
- import { BrowserRouter, Route, Switch } from "react-router-dom";
->>>>>>> 483590e587cf95be1c0480117975e5e86c30faa9
+
+
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -28,14 +24,11 @@ import RegisterProperty from "./pages/RegisterProperty/RegisterProperty";
 import EditProfile from "./pages/User/EditProfile/EditProfile";
 import Booking from "./pages/BookingProperty/Booking";
 import Sell from "./pages/Sell/Sell";
-<<<<<<< HEAD
-import propertyDetails from "./pages/Sell/propertyDetails";
-=======
-import ProjectProperty from './pages/BookingProperty/ProjectProperty';
-import ProjectDetail from './pages/BookingProperty/ProjectDetail';
-import sellDetail from './pages/Sell/sellDetail';
+import propertyDetails from "./pages/Sell/propertyDetails"
+import sellDetail from "./pages/Sell/sellDetail"
+import ProjectProperty from "./pages/BookingProperty/ProjectProperty"
+import ProjectDetail from "./pages/BookingProperty/ProjectDetail"
 
->>>>>>> b49c442982fc9dc3d37545f71bb4cc7a6ec68846
 
 
 
@@ -63,21 +56,21 @@ function App() {
           <Route exact path="/EditProfile" component={EditProfile} />
           <Route exact path="/book-property" component={Booking} />
           <Route exact path="/sell" component={Sell} />
-<<<<<<< HEAD
+
           <Route exact path="/About" component={About} />
           <Route exact path="/Blog" component={Blog} />
+          <Route exact path="/Blogs/Blogdata/:id" component={BlogViewDetails} />
 
-=======
-<<<<<<< HEAD
+
           <Route exact path="/property/:id" component={propertyDetails} />
-=======
+
           <Route exact path="/sellDetail/:id" component={sellDetail} />
           <Route exact path="/projects" component={ProjectProperty} />
           <Route exact path="/projectDetail/:id" component={ProjectDetail} />
->>>>>>> b49c442982fc9dc3d37545f71bb4cc7a6ec68846
->>>>>>> 483590e587cf95be1c0480117975e5e86c30faa9
 
-          
+
+
+
         </Switch>
         <Footer />
       </BrowserRouter>
